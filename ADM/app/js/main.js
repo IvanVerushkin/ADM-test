c6fd9153b4ce53e1299d6c37a7ac2727d2c54e0d
IvanVerushkin <tenekrut30@gmail.com>
2021-10-21 16:23:25 +0300

    21.10 start of adaptave

--- ADM/app/js/main.js
+++ ADM/app/js/main.js
@@ -1,11 +1,14 @@
-let swiper = new Swiper('.swiper-container', {
-    wrapperClass: 'swiper-wrapper',
+const swiper = document.querySelector('.swiper-container');
+const swiper2 = document.querySelector('.swiper-container2');
+
+let Myswiper1 = new Swiper('.slider-conteiner', {
+    wrapperClass: 'slider-wrapper',
     slideClass: 'swiper-item',
     slidesPerView: 1,
     loop: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
     },
     autoplay: {
         stopOnLastSlide: false,
@@ -15,4 +18,19 @@ let swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true
     }
+});
+
+let Myswiper2 = new Swiper('.customers__container', {
+    wrapperClass: 'customers__wrapper',
+    slideClass: 'customers__item',
+    slidesPerView: 6,
+    loop: true,
+    navigation: {
+        nextEl: '.customers-button-next',
+        prevEl: '.customers-button-prev',
+    },
+    // autoplay: {
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false
+    // }
 });
